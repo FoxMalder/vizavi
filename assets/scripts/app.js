b1de854bba28fcdeb1de09c601a0ec68da3ead0a
Sergey Glazov <yaglazov@gmail.com>
2019-07-16 00:20:39 +0300

    партнеры

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -88,9 +88,330 @@
       }
     });
   }
-})(jQuery); // = modules/graph.js
+})(jQuery);
 
-
+$(function () {
+  $('#graph-container_1').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_2').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_3').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_4').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_5').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_6').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_7').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_8').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_9').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_10').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+});
 $(document).ready(function () {
   var owl = $('.owl-carousel');
   var oLeft = $('.js_slider-left');
