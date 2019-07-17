a73d2f407dc32e4b51946f048e30cdc47e230194
Sergey Glazov <yaglazov@gmail.com>
2019-07-17 19:38:32 +0300

    партнерам

--- app/scripts/app/modules/slider.js
+++ app/scripts/app/modules/slider.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
 
-  var owl    = $('.owl-carousel');
-  var oLeft  = $('.js_slider-left');
-  var oRight = $('.js_slider-right');
+  var owl          = $('.js_traders-list');
+  var owlLeft      = owl.parents('.traders-slider__list').find('.js_slider-left');
+  var owlRight     = owl.parents('.traders-slider__list').find('.js_slider-right');
 
   owl.owlCarousel({
     loop:true,
     items:4
   });
-
-  oLeft.click(function(){
+  owlLeft.click(function(){
     owl.trigger('prev.owl.carousel');
   });
-
-  oRight.click(function(){
+  owlRight.click(function(){
     owl.trigger('next.owl.carousel');
   });
 
+  var owl2          = $('.js_advantage-slider');
+  var owlLeft2      = owl2.parents('.text-investor__cards').find('.js_slider-left');
+  var owlRight2     = owl2.parents('.text-investor__cards').find('.js_slider-right');
+
+  owl2.owlCarousel({
+    loop:true,
+    margin:30,
+    items:4
+  });
+  owlLeft2.click(function(){
+    owl2.trigger('prev.owl.carousel');
+  });
+  owlRight2.click(function(){
+    owl2.trigger('next.owl.carousel');
+  });
 });
