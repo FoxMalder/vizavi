8a5784a6ff18242e6675ba08b229980b59965348
Sergey Glazov <yaglazov@gmail.com>
2019-08-12 18:01:01 +0300

    переменные для мобил переименовал

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,228 @@
-"use strict";!function(i){var s=i(".js_header-link"),t=i(".js_header__cloud"),a=i(".header__cloud-item"),e=(i(".js_header-finder"),i(".header__finder-item")),l=i(".js_user-action"),r=i(".js_header-user"),o=i(".js_search-action"),n=i(".js_header-search");s.each(function(){i(this).on("click",function(e){(i(".js_user-action.is-active")||i(".js_search-action.is-active"))&&(l.removeClass("is-active"),o.removeClass("is-active"));var s=i(this).attr("data-href");i(this).closest(".header-menu__item").siblings().removeClass("is-active"),i(this).closest(".header-menu__item").addClass("is-active"),t.addClass("is-show"),a.removeClass("is-show"),i(s).siblings(".header__finder-item").removeClass("is-show"),i(s).addClass("is-show"),e.preventDefault()})}),l.on("click",function(){i(this).toggleClass("is-active"),t.addClass("is-show"),e.removeClass("is-show"),r.siblings(".header__cloud-item").removeClass("is-show"),r.addClass("is-show")}),o.on("click",function(){i(this).toggleClass("is-active"),t.addClass("is-show"),e.removeClass("is-show"),n.siblings(".header__cloud-item").removeClass("is-show"),n.addClass("is-show")}),i(".js_header__cloud.is-show")&&i(document).on("keyup",function(e){27===e.keyCode&&(s.closest(".header-menu__item").siblings().removeClass("is-active"),t.removeClass("is-show"),r.removeClass("is-show"))}),i(".js_header-toggle").click(function(e){e.preventDefault(),i(this).toggleClass("is-active")})}(jQuery),function(s){var i=s(".js_language-action"),t=s(".js_language-list");i.click(function(e){e.preventDefault(),s(this).toggleClass("is-open"),t.toggleClass("is-show")}),s(".js_language-list.is-show")&&(s(document).on("keyup",function(e){27===e.keyCode&&(i.removeClass("is-open"),t.removeClass("is-show"))}),s(document).mouseup(function(e){0===t.has(e.target).length&&(i.removeClass("is-open"),t.removeClass("is-show"))}))}(jQuery),$(document).ready(function(){$("#trading-market-content").easytabs({animate:!0,animationSpeed:"fast",defaultTab:"span#is-select",tabs:"> div > div > span",tabActiveClass:"is-active",panelActiveClass:"active-content-div",updateHash:!1})}),$(function(){$("#graph-container_1").highcharts({chart:{type:"area"},title:{text:null,enabled:!1},xAxis:{labels:{enabled:!1}},yAxis:{title:{text:null}},legend:{enabled:!1},credits:{enabled:!1},tooltip:{shared:!1},series:[{showInLegend:!1,data:[5,3,8,2,4]}]}),$("#graph-container_2").highcharts({chart:{type:"area"},title:{text:null,enabled:!1},xAxis:{labels:{enabled:!1}},yAxis:{title:{text:null}},legend:{enabled:!1},credits:{enabled:!1},tooltip:{shared:!1},series:[{showInLegend:!1,data:[5,3,8,2,4]}]})}),$(document).ready(function(){var e=$(".js_traders-list"),s=e.parents(".traders-slider__list").find(".js_slider-left"),i=e.parents(".traders-slider__list").find(".js_slider-right");e.owlCarousel({loop:!0,items:4}),s.click(function(){e.trigger("prev.owl.carousel")}),i.click(function(){e.trigger("next.owl.carousel")});var t=$(".js_advantage-slider"),a=t.parents(".text-investor__cards").find(".js_slider-left"),l=t.parents(".text-investor__cards").find(".js_slider-right");t.owlCarousel({loop:!1,margin:30,items:4}),a.click(function(){t.trigger("prev.owl.carousel")}),l.click(function(){t.trigger("next.owl.carousel")});var r=$(".js_traders-list-main"),o=r.parents(".top-traders__list").find(".js_slider-left"),n=r.parents(".top-traders__list").find(".js_slider-right");r.owlCarousel({loop:!0,margin:20,items:3}),o.click(function(){r.trigger("prev.owl.carousel")}),n.click(function(){r.trigger("next.owl.carousel")}),$(".js_headline-slider").owlCarousel({loop:!1,dots:!0,items:1,center:!0})});
+"use strict";
+
+(function ($) {
+  var menuItem = $('.js_header-link');
+  var menuCloud = $('.js_header__cloud');
+  var menuCloudItem = $('.header__cloud-item');
+  var menuFinder = $('.js_header-finder');
+  var menuFinderItem = $('.header__finder-item');
+  var menuItemUser = $('.js_user-action');
+  var menuCloudUser = $('.js_header-user');
+  var menuItemSearch = $('.js_search-action');
+  var menuCloudSearch = $('.js_header-search');
+  menuItem.each(function () {
+    $(this).on('click', function (event) {
+      if ($('.js_user-action.is-active') || $('.js_search-action.is-active')) {
+        menuItemUser.removeClass('is-active');
+        menuItemSearch.removeClass('is-active');
+      }
+
+      var href = $(this).attr('data-href');
+      $(this).closest('.header-menu__item').siblings().removeClass('is-active');
+      $(this).closest('.header-menu__item').addClass('is-active');
+      menuCloud.addClass('is-show');
+      menuCloudItem.removeClass('is-show');
+      $(href).siblings('.header__finder-item').removeClass('is-show');
+      $(href).addClass('is-show');
+      event.preventDefault();
+    });
+  });
+  menuItemUser.on('click', function () {
+    $(this).toggleClass('is-active');
+    menuCloud.addClass('is-show');
+    menuFinderItem.removeClass('is-show');
+    menuCloudUser.siblings('.header__cloud-item').removeClass('is-show');
+    menuCloudUser.addClass('is-show');
+  });
+  menuItemSearch.on('click', function () {
+    $(this).toggleClass('is-active');
+    menuCloud.addClass('is-show');
+    menuFinderItem.removeClass('is-show');
+    menuCloudSearch.siblings('.header__cloud-item').removeClass('is-show');
+    menuCloudSearch.addClass('is-show');
+  }); // закрытие по ESC и клику в любой области
+
+  if ($('.js_header__cloud.is-show')) {
+    $(document).on('keyup', function (event) {
+      if (event.keyCode === 27) {
+        menuItem.closest('.header-menu__item').siblings().removeClass('is-active');
+        menuCloud.removeClass('is-show');
+        menuCloudUser.removeClass('is-show');
+      }
+    });
+    /*
+    $(document).mouseup(function (event) {
+      if ( menuCloud.has(event.target).length === 0 ) {
+        menuItem
+          .closest('.header-menu__item')
+          .siblings()
+          .removeClass('is-active');
+        menuCloud.removeClass('is-show');
+        menuCloudUser.removeClass('is-show');
+      }
+    });
+    */
+  } // клик по гамбургеру
+
+
+  (function () {
+    $('.js_header-toggle').click(function (e) {
+      e.preventDefault();
+      $(this).toggleClass('is-active'); // $('.top-menu').toggleClass('open');
+    });
+  })();
+})(jQuery);
+
+(function ($) {
+  var languageHeader = $('.js_language-action');
+  var languageList = $('.js_language-list');
+  languageHeader.click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass('is-open');
+    languageList.toggleClass('is-show');
+  });
+
+  if ($('.js_language-list.is-show')) {
+    $(document).on('keyup', function (event) {
+      if (event.keyCode === 27) {
+        languageHeader.removeClass('is-open');
+        languageList.removeClass('is-show');
+      }
+    });
+    $(document).mouseup(function (event) {
+      if (languageList.has(event.target).length === 0) {
+        languageHeader.removeClass('is-open');
+        languageList.removeClass('is-show');
+      }
+    });
+  }
+})(jQuery);
+
+$(document).ready(function () {
+  $('#trading-market-content').easytabs({
+    animate: true,
+    animationSpeed: 'fast',
+    defaultTab: "span#is-select",
+    tabs: '> div > div > span',
+    tabActiveClass: 'is-active',
+    panelActiveClass: "active-content-div",
+    updateHash: false
+  });
+});
+$(function () {
+  $('#graph-container_1').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+  $('#graph-container_2').highcharts({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: null,
+      enabled: false
+    },
+    xAxis: {
+      labels: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: null
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shared: false
+    },
+    series: [{
+      showInLegend: false,
+      data: [5, 3, 8, 2, 4]
+    }]
+  });
+});
+$(document).ready(function () {
+  var owl = $('.js_traders-list');
+  var owlLeft = owl.parents('.traders-slider__list').find('.js_slider-left');
+  var owlRight = owl.parents('.traders-slider__list').find('.js_slider-right');
+  owl.owlCarousel({
+    loop: true,
+    items: 4
+  });
+  owlLeft.click(function () {
+    owl.trigger('prev.owl.carousel');
+  });
+  owlRight.click(function () {
+    owl.trigger('next.owl.carousel');
+  });
+  var owl2 = $('.js_advantage-slider');
+  var owlLeft2 = owl2.parents('.text-investor__cards').find('.js_slider-left');
+  var owlRight2 = owl2.parents('.text-investor__cards').find('.js_slider-right');
+  owl2.owlCarousel({
+    loop: false,
+    margin: 30,
+    items: 4
+  });
+  owlLeft2.click(function () {
+    owl2.trigger('prev.owl.carousel');
+  });
+  owlRight2.click(function () {
+    owl2.trigger('next.owl.carousel');
+  });
+  var owl3 = $('.js_traders-list-main');
+  var owlLeft3 = owl3.parents('.top-traders__list').find('.js_slider-left');
+  var owlRight3 = owl3.parents('.top-traders__list').find('.js_slider-right');
+  owl3.owlCarousel({
+    loop: true,
+    margin: 20,
+    items: 3
+  });
+  owlLeft3.click(function () {
+    owl3.trigger('prev.owl.carousel');
+  });
+  owlRight3.click(function () {
+    owl3.trigger('next.owl.carousel');
+  });
+  var owl4 = $('.js_headline-slider');
+  owl4.owlCarousel({
+    loop: false,
+    dots: true,
+    items: 1,
+    center: true
+  });
+});
+//# sourceMappingURL=maps/app.js.map
